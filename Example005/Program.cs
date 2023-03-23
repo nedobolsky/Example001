1af8d2a9192bc83a72a74ba0198faba4781926d9
Dmitry Nedobolsky <t0988@yandex.ru>
2023-03-23 15:00:36 +0300

    Ветвление

---
+++ Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Как тебя зовут? ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша! ");
+}
+
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
